e3ee8011f0777e5062e9c4ff3089a5a150962b7f
Lout33 <sssimple.33@gmail.com>
2021-01-30 21:01:19 -0500

    improve imagen

--- main.py
+++ main.py
@@ -2,61 +2,96 @@ from PIL import Image, ImageDraw, ImageFilter , ImageFont
 import requests
 
 
-#  sacar la lista de urls de la db 
-cesarUrlImage = 'https://declara.jne.gob.pe/Assets/Fotos-HojaVida/136626.jpg?1611899645675'
-im2 = Image.open(requests.get(cesarUrlImage, stream=True).raw)
-
-# get Data
-cesarUrlData = 'https://api-dev.candidatos.pe/v1/candidato/34/?format=json'
-data = requests.get(cesarUrlData)
-data1 = data.json()
-
-
 # create Canvas 
-img = Image.new('RGB', (500, 300), (230, 230, 230))
+canvas = Image.new('RGB', (400, 250), (230, 230, 230))
+draw = ImageDraw.Draw(canvas)
 
+#####################
+#  sacar la lista de urls de CANDIDATO de la db 
+cesarUrlImage = 'https://declara.jne.gob.pe/Assets/Fotos-HojaVida/136626.jpg?1611899645675'
+candidatoImg = Image.open(requests.get(cesarUrlImage, stream=True).raw)
 
 # insert image candidato
-  #resize
-resized_im = im2.resize((round(im2.size[0]*0.5), round(im2.size[1]*0.5)))
-
-draw = ImageDraw.Draw(img)
+# resize
+resized_candidato = candidatoImg.resize((round(candidatoImg.size[0]*0.25), round(candidatoImg.size[1]*0.25)))
+canvas.paste(resized_candidato,(20, 20))
 # draw.line((200, 100, 300, 200), fill=(0, 0, 0), width=10)
 
-img.paste(resized_im,(50, 50))
+#####################
+
+#  sacar la lista de urls de PARTIDO de la db 
+partidoUrlImage = 'https://aplicaciones007.jne.gob.pe/srop_publico/Consulta/Simbolo/GetSimbolo/1257'
+partidoImg = Image.open(requests.get(partidoUrlImage, stream=True).raw)
+
+# insert image partido
+  #resize
+resized_partido = partidoImg.resize((round(partidoImg.size[0]*0.15), round(partidoImg.size[1]*0.15)))
+# draw = ImageDraw.Draw(canvas)
+canvas.paste(resized_partido,(80, 100))
+# draw.line((200, 100, 300, 200), fill=(0, 0, 0), width=10)
+
+#####################
+
+# import images and icons 
+museum = Image.open("museum.png")
+like = Image.open("like.png")
+dislike = Image.open("dislike.png")
+description = Image.open("description.png")
 
 
+resized_museum = museum.resize((round(museum.size[0]*0.08), round(museum.size[1]*0.08)))
+canvas.paste(resized_museum,(20, 160),resized_museum)
+
+
+
+# get TEXT Data
+# CONSTATNS 
+RED = (255, 0, 0)
+WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
+
+# 
+
+font10 = ImageFont.truetype('Roboto-Light.ttf', 10)
+font12 = ImageFont.truetype('Roboto-Light.ttf', 12)
+font14 = ImageFont.truetype('Roboto-Light.ttf', 14)
+
+font16 = ImageFont.truetype('Roboto-Light.ttf', 16)
+font20 = ImageFont.truetype('Roboto-Light.ttf', 20)
+font30 = ImageFont.truetype('Roboto-Light.ttf', 30)
+
+cesarUrlData = 'https://api-dev.candidatos.pe/v1/candidato/34/?format=json'
+requestData = requests.get(cesarUrlData)
+data = requestData.json()
 
 # Insert Text to image
-
-#Create an Image Object from an Image
-width, height = img.size
-
-draw = ImageDraw.Draw(img)
-text1 = data1["id"]
-text2 = data1["jne_organizacion_politica"]
-text3 = data1["nombres"]
-text4 = data1["profesion"]
+# Create an Image Object from an Image
+width, height = canvas.size
+draw = ImageDraw.Draw(canvas)
 
 
-# font = ImageFont.load_default()
-# font = ImageFont.truetype( 36)
-font = ImageFont.truetype('Roboto-Light.ttf', 1000)
-# textwidth, textheight = draw.textsize(text, font)
+text1 = data["jne_organizacion_politica"]
+text2 = f'{data["nombres"]} {data["apellido_paterno"]} {data["apellido_materno"]}'
 
-# calculate the x,y coordinates of the text
-# margin = 10
-# x = width - textwidth - margin
-# y = height - textheight - margin
 
 # draw watermark in the bottom right corner
-draw.text((300, 100), str(text1), fill=(255, 0, 0))
-draw.text((300, 120), str(text2), fill=(255, 0, 0))
-draw.text((300, 140), str(text3), fill=(255, 0, 0))
-draw.text((300, 160), str(text4), fill=(255, 0, 0))
-
-img.show()
+draw.text((125, 20), str(text1), fill=BLACK,font=font10)
+draw.text((125, 40), str(text2), fill=BLACK,font=font20)
 
 
+text3 = data["profesion"]
 
-# img.show()
+import textwrap
+lines = textwrap.wrap(text3, width=30)
+y_text = 80
+for line in lines:
+  line_width, line_height = font14.getsize(line)
+  draw.text((125 , y_text), line, fill=BLACK,font=font14)
+  y_text += line_height + 5  # add spacer bottom
+
+
+
+
+canvas.show()
+
+
